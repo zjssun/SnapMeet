c6a085e87708c3b3c935e9b5280823c821513a0b
SamRol <1628526495@qq.com>
2025-12-02 12:19:48 +0800

    Init

---
+++ src/main/java/com/example/snapmeet/SnapmeetApplication.java
@@ -0,0 +1,13 @@
+package com.example.snapmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapmeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapmeetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/snapmeet/SnapmeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.snapmeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapmeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
