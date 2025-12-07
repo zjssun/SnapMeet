b3929a71d84f841eeefed4276762ae54cf3f97bd
SamRol <1628526495@qq.com>
2025-12-07 22:21:46 +0800

    COMMIT

--- src/main/java/com/example/snapmeet/SnapmeetApplication.java
+++ src/main/java/com/example/snapmeet/SnapmeetApplication.java
@@ -1,9 +1,11 @@
 package com.example.snapmeet;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.snapmeet.mapper")
 public class SnapmeetApplication {
 
 	public static void main(String[] args) {
